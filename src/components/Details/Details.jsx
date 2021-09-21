fa0f9937d85e489ed10b4e8ddcc7bbee395e3d52
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-21 08:19:21 +0800

    Created custom hooks

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core'
 import { Doughnut } from 'react-chartjs-2'
+import useTransactions from '../../useTransactions'
 
 import useStyles from './styles'
 
 const Details = ({ title }) => {
     const classes = useStyles();
+    const { total, chartData } = useTransactions(title)
 
     return (
         <Card className={title === 'Income' ? classes.income: classes.expense}>
             <CardHeader title={title}/>
             <CardContent>
                 <Typography variant="h5">
-                    $50
+                    ₱{total}
                 </Typography>
-                {/* <Doughnut data="DATA"/> */}
+                <Doughnut data={chartData}/>
             </CardContent>
         </Card>
     )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,10 +11,10 @@ const Main = () => {
         <Card className={classes.root}>
             <CardHeader title="Budget Tracker" subheader="Powered by Speechly"/>
             <CardContent>
-                <Typography align="center" variant="h5">Total Balance: $100</Typography>
+                <Typography align="center" variant="h5">Total Balance: ₱100</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
                     {/* InfoCard .... */}
-                    Try saying: $100 for this category at this particular moment of time
+                    Try saying: ₱100 for this category at this particular moment of time
                 </Typography>
                 <Divider/>
                     <Form/>
