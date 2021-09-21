db2b8417387b3a4eb3545c9738a5319768bb3ed3
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-21 11:20:31 +0800

    Total balance

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core'
+import { ExpenseTrackerContext } from '../../context/context'
 
 import Form from './Form/Form'
 import List from './List/List'
@@ -7,11 +8,12 @@ import useStyles from './styles'
 
 const Main = () => {
     const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext)
     return (
         <Card className={classes.root}>
             <CardHeader title="Budget Tracker" subheader="Powered by Speechly"/>
             <CardContent>
-                <Typography align="center" variant="h5">Total Balance: ₱100</Typography>
+                <Typography align="center" variant="h5">Total Balance: ₱{balance}</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
                     {/* InfoCard .... */}
                     Try saying: Add EXPENSE of 50 pesos in category TRAVEL for THURSDAY
