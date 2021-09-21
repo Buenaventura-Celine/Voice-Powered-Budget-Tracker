c37484edec9e460b6e700cb6ba91af5c365c2620
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-21 11:54:16 +0800

    Scroll to main in mobile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Grid } from '@material-ui/core'
 import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui'
+import { SpeechState, useSpeechContext } from '@speechly/react-client'
 
 import Details from './components/Details/Details'
 import Main from './components/Main/Main'
@@ -8,6 +9,16 @@ import useStyles from './styles'
 
 const App = () => {
     const classes = useStyles();
+    const { speechState } = useSpeechContext();
+    const main = useRef(null);
+
+    const executeScroll = () => main.current.scrollIntoView();
+
+    useEffect(() => {
+        if(speechState === SpeechState.Recording ){
+            executeScroll();
+        }
+    }, [speechState])
 
     const func = () =>{
         var context =  new AudioContext();
@@ -22,7 +33,7 @@ const App = () => {
                 <Grid item xs={12} sm={4} className={classes.mobile}>
                     <Details title="Income"/>
                 </Grid>
-                <Grid item xs={12} sm={3} className={classes.main}>
+                <Grid ref={main} item xs={12} sm={3} className={classes.main}>
                     <Main/>
                 </Grid>
                 <Grid item xs={12} sm={4} className={classes.desktop}>
