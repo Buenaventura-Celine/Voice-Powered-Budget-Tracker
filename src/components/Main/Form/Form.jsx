6071546e9cd64a95c6b24c75b93095f891cf851e
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-21 11:28:28 +0800

    Implementing Snackbar

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -7,6 +7,7 @@ import formatDate from '../../../utils/formatDate'
 import { ExpenseTrackerContext } from '../../../context/context'
 import { incomeCategories, expenseCategories } from '../../../constants/categories'
 import useStyles from './styles'
+import CustomizedSnackbar from '../../Snackbar/Snackbar'
 
 const initialState = {
     amount: '',
@@ -20,10 +21,12 @@ const Form = () => {
     const [formData, setFormData] = useState(initialState)
     const { addTransaction } = useContext(ExpenseTrackerContext)
     const { segment } = useSpeechContext()
+    const [open, setOpen] = useState(false);
 
     const createTransaction = () =>{
         if (Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
         const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
+        setOpen(true);
         addTransaction(transaction)
         setFormData(initialState)
     }
@@ -75,6 +78,7 @@ const Form = () => {
 
     return (
         <Grid container spacing={2}>
+            <CustomizedSnackbar open={open} setOpen={setOpen}/>
             <Grid item xs={12}>
                 <Typography align="center" variant="subtitle2" gutterBottom>
                     {segment && segment.words.map((w) => w.value).join(" ")}
